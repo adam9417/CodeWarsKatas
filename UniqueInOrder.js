12f34ca9a5fe89dfefee260911df13c74c4557f5
Adam Derbidge <adam9417@gmail.com>
2021-08-31 15:43:49 -0400

    Unique in Order kata complete

---
+++ UniqueInOrder.js
@@ -0,0 +1,21 @@
+function uniqueInOrder(iterable) {
+  let strArr = Array.isArray(iterable) ? iterable : iterable.split("");
+  let unique = strArr.filter((letter, i) => {
+    return strArr[i] !== strArr[i + 1];
+  });
+  return unique;
+}
+
+describe("Tests", () => {
+  it("test", () => {
+Test.assertSimilar(uniqueInOrder('AAAABBBCCDAABBB'), ['A','B','C','D','A','B'])
+  });
+});
+
+/* Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements with the same value next to each other and preserving the original order of elements.
+
+For example:
+
+uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
+uniqueInOrder([1,2,2,3,3])       == [1,2,3] */ 
