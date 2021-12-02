338fa141e8773fcd1b6938d51c5e5aed1e613272
Adam Derbidge <adam9417@gmail.com>
2021-12-02 14:17:28 -0500

    Boolean to String kata complete

---
+++ BooleanToString.js
@@ -0,0 +1,14 @@
+function booleanToString(b){
+  return b ? 'true' : 'false';
+}
+
+describe("Tests", () => {
+  it("test", () => {
+Test.assertEquals(booleanToString(true), "true", 'When we pass in true, we want the string "true" as output');
+Test.assertEquals(booleanToString(false), "false", 'When we pass in false, we want the string "false" as output');
+  });
+});
+
+//Implement a function which convert the given boolean value into its string representation.
+
+//Note: Only valid inputs will be given.
